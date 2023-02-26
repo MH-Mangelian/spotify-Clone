83664abfd663d3d5d8b40582f0da0c9d227c1df5
Mohammad Hossein Mangelian <h.m.mnglean@gmail.com>
2023-02-26 12:56:43 +0330

    API Crash  can't buy

--- src/Redux/services/SpotifyApi.js
+++ src/Redux/services/SpotifyApi.js
@@ -4,19 +4,20 @@ const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'a76a3c7e26msh4d9ab3817a6caaep11e0e6jsnbc8cb8677794',
-		'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+		'X-RapidAPI-Host': 'spotify-downloader1.p.rapidapi.com'
 	}
 };
 
-fetch('https://spotify81.p.rapidapi.com/track_credits?id=4WNcduiCmDNfmTEz7JvmLv', options)
+fetch('https://spotify-downloader1.p.rapidapi.com/trackList/playlist/37i9dQZF1DX0XUsuxWHRQd', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+   
 
 export const SpotifyApi = createApi({
   reducerPath: "SpotifyApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://spotify81.p.rapidapi.com/track_credits",
+    baseUrl: "https://spotify-downloader1.p.rapidapi.com/trackList",
     prepareHeaders: (headers)=>{
       headers.set('X-RapidAPI-Key', 'a76a3c7e26msh4d9ab3817a6caaep11e0e6jsnbc8cb8677794');
 
@@ -24,7 +25,7 @@ export const SpotifyApi = createApi({
     },
   }),
   endpoints: (builder) => ({
-         getTopCharts: builder.query({ query: () => 'track_credits?id=4WNcduiCmDNfmTEz7JvmLv' }),
+         getTopCharts: builder.query({ query: () => '/playlist/37i9dQZF1DX0XUsuxWHRQd' }),
          getSongsByGenre: builder.query({ query: (genre) => `${genre}` }),
          getSongsByCountry: builder.query({ query: (countryCode) => `${countryCode}` }),
          getSongsBySearch: builder.query({ query: (searchTerm) => `${searchTerm}` }),
@@ -32,7 +33,9 @@ export const SpotifyApi = createApi({
          getSongDetails: builder.query({ query: ({ songid }) => `${songid}` }),
          getSongRelated: builder.query({ query: ({ songid }) => `${songid}` }),
       }),
+    
 })
+
 
 export const {
    useGetTopChartsQuery,
